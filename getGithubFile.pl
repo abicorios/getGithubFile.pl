f55f855135dd87dec4db4ed58eb4e1d573ba906d
abicorios <yaroslav.moroz91@gmail.com>
2018-10-13 16:24:50 +0300

    up accuracy

--- getGithubFile.pl
+++ getGithubFile.pl
@@ -1,12 +1,11 @@
 #!/usr/bin/env perl
-if(-e '/usr/ports') {
-	$l='fetch';
-} else {
-	$l='wget';
+$l='fetch';
+sub installed {
+	$s=pop @_;
+	return length(`whereis $s`)>length("$s: /bin/$s");
 }
-$_=`whereis $l`;
-s/$l: ?//;
-die "install $l, please\n" if length($_)<length("/bin/$l");
+$l=installed('fetch')?'fetch':'wget';
+die "install $l, please\n" if !installed($l);
 die "enter 1 argument - ref to file in rep\n" if @ARGV!=1;
 $_=$ARGV[0];
 s/github\.com/raw.githubusercontent\.com/;
