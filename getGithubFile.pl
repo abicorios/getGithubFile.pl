40afa3f090c6beca7d45a53a69788396500a4da6
abicorios <yaroslav.moroz91@gmail.com>
2018-10-13 18:33:07 +0300

    add freebsd support

--- getGithubFile.pl
+++ getGithubFile.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/env perl
-$_=`whereis wget`;
-s/wget: ?//;
-die "install wget, please\n" if length($_)<length('/bin/wget');
+if(-e '/usr/ports') {
+	$l='fetch';
+} else {
+	$l='wget';
+}
+$_=`whereis $l`;
+s/$l: ?//;
+die "install $l, please\n" if length($_)<length("/bin/$l");
 die "enter 1 argument - ref to file in rep\n" if @ARGV!=1;
 $_=$ARGV[0];
 s/github\.com/raw.githubusercontent\.com/;
 s!/blob/!/!;
-`wget $_`;
+`$l $_`;
