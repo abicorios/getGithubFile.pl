7e7fda8fc7fbc111f4edf1cff54cd20ff481b55a
abicorios <yaroslav.moroz91@gmail.com>
2018-10-14 10:01:20 +0300

    compactification of code

--- getGithubFile.pl
+++ getGithubFile.pl
@@ -1,11 +1,6 @@
 #!/usr/bin/env perl
-sub installed {
-	$s=pop @_;
-	return length(`whereis $s`)>length("$s: /bin/$s");
-}
-$l='fetch';
-$l='wget' if not installed 'fetch';
-die "install $l, please\n" if !installed($l);
+sub p {$_=`whereis $_[0]`;@w=split;$w[1];}
+$l=p('fetch') || p('wget') || die "install wget, please\n";
 die "enter 1 argument - ref to file in rep\n" if @ARGV!=1;
 $_=$ARGV[0];
 s/github\.com/raw.githubusercontent\.com/;
